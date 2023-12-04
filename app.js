afab71aeef97f336dc09829e4251f39661f03ad6
hrishikesh <hrishikesh.malagi@kleit.ac.in>
2023-12-04 20:47:43 +0530

    fixed db handeling

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = 3000;
-
-// Database connection details
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // Replace with your actual database username
-    password: '', // Replace with your actual database password
-    database: 'elearn', // Replace with your actual database name
-});
-
-// Connect to the database
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL database: ' + err.stack);
-        process.exit(1); // Exit the application if unable to connect to the database
-    }
-    console.log('Connected to MySQL database');
-});
 
 // Middleware to parse JSON and url-encoded form data
 app.use(bodyParser.json());
@@ -28,32 +12,66 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve HTML form
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/Contact.html');
+    res.sendFile(path.join(__dirname, 'Contact.html'));
 });
 
+// Database connection pool
+const pool = mysql.createPool({
+    connectionLimit: 10, // Adjust the limit based on your requirements
+    host: 'localhost',
+    user: 'root',
+    password: 'HrishiM$943',
+    database: 'elearn',
+});
+
+// Handle form submissions
 // Handle form submissions
 app.post('/process_contact_form', (req, res) => {
     const { name, email, phone, message } = req.body;
 
-    // SQL query to insert data into the table
-    const sql = "INSERT INTO contact_form (name, email, phone, message) VALUES (?, ?, ?, ?)";
-
-    // Execute the query with parameterized values to prevent SQL injection
-    db.query(sql, [name, email, phone, message], (err) => {
+    // Use the connection pool for handling database connections
+    pool.getConnection((err, connection) => {
         if (err) {
-            console.error('Error executing SQL query: ' + err.message);
-            res.status(500).send('Internal Server Error');
+            console.error('Error getting MySQL connection:', err.message);
+            res.status(500).json({ success: false, message: 'Internal Server Error' });
             return;
         }
-        console.log('Form data submitted successfully!');
 
+        // SQL query to insert data into the table
+        const sql = 'INSERT INTO contact_form (name, email, phone, message) VALUES (?, ?, ?, ?)';
 
-        // Show an alert and then redirect
-        res.send('<script>alert("Thanks! We will contact you soon."); window.location.href="http://localhost:3000/Contact.html";</script>');
+        // Use parameterized queries to prevent SQL injection
+        connection.query(sql, [name, email, phone, message], (err, results) => {
+            connection.release(); // Release the connection back to the pool
+
+            if (err) {
+                console.error('Error executing SQL query:', err.message);
+                res.status(500).json({ success: false, message: 'Internal Server Error' });
+                return;
+            }
+
+            console.log('Form data submitted successfully!');
+            // Redirect to Contact.html after successful form submission
+            res.json({ success: true, message: 'Thanks! We will contact you soon.', redirect: '/Contact.html' });
+        });
     });
 });
 
 // Start the server
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
+});
+
+// Handle server shutdown
+process.on('SIGINT', () => {
+    console.log('Server is shutting down...');
+    server.close(() => {
+        console.log('Server closed.');
+
+        // Optionally, close the database connection pool gracefully here
+        pool.end(() => {
+            console.log('Database connection pool closed.');
+            process.exit(0);
+        });
+    });
 });
